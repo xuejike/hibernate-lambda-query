bb1615cec19bb1a799672eb8010c38171ad7f53f
xuejike <xuejike2004@126.com>
2017-03-10 17:14:02 +0800

    修复 ne bug

--- src/test/java/test/TestMain.java
+++ src/test/java/test/TestMain.java
@@ -7,50 +7,50 @@ import com.bidanet.hibernate.lambda.core.LambdaCriteria;
  */
 public class TestMain {
     public static void main(String[] arg){
-        // eq 查询
-        LambdaCriteria<User> lambdaCriteria = new LambdaCriteria<>(User.class, session);
-        // select * from user where name = 'name' and sex = '男'
-        lambdaCriteria.eq(query ->{
-            query.setName("name");
-            query.setSex("男");
-        }).list();
-
-
-        lambdaCriteria = new LambdaCriteria<>(User.class, session);
-        // select * from user where name = 'name' and ISNULL(sex)
-        lambdaCriteria.eq(query ->{
-            query.setName("name");
-            query.setSex(null);
-        }).list();
-
-
-        lambdaCriteria = new LambdaCriteria<>(User.class, session);
-        // like 查询
-        //select * from user where name like '%name%'
-        lambdaCriteria.like(query ->{
-            query.setName("%name%");
-        }).list();
-
-        lambdaCriteria = new LambdaCriteria<>(User.class, session);
-        // ne 查询
-        //select * from user where name <> 'name' and name <> 'pp'
-        lambdaCriteria.ne(query -> {
-            query.setName("name");
-            query.setName("pp");
-        }).list();
-
-        lambdaCriteria = new LambdaCriteria<>(User.class, session);
-        // 组合使用
-        // sselect * from user where open =1 and name like '%xx%' and sex <> '女'
-        lambdaCriteria
-                .eq(query -> query.setOpen(true))
-                .like(query -> query.setName("%xx%"))
-                .ne(query -> query.setSex("女")).list();
-//        分页  起始值 1
-        lambdaCriteria = new LambdaCriteria<>(User.class, session);
-        lambdaCriteria
-                .eq(query -> query.setOpen(true))
-                .like(query -> query.setName("%xx%"))
-                .ne(query -> query.setSex("女")).list(1,10);
+//        // eq 查询
+//        LambdaCriteria<User> lambdaCriteria = new LambdaCriteria<>(User.class, session);
+//        // select * from user where name = 'name' and sex = '男'
+//        lambdaCriteria.eq(query ->{
+//            query.setName("name");
+//            query.setSex("男");
+//        }).list();
+//
+//
+//        lambdaCriteria = new LambdaCriteria<>(User.class, session);
+//        // select * from user where name = 'name' and ISNULL(sex)
+//        lambdaCriteria.eq(query ->{
+//            query.setName("name");
+//            query.setSex(null);
+//        }).list();
+//
+//
+//        lambdaCriteria = new LambdaCriteria<>(User.class, session);
+//        // like 查询
+//        //select * from user where name like '%name%'
+//        lambdaCriteria.like(query ->{
+//            query.setName("%name%");
+//        }).list();
+//
+//        lambdaCriteria = new LambdaCriteria<>(User.class, session);
+//        // ne 查询
+//        //select * from user where name <> 'name' and name <> 'pp'
+//        lambdaCriteria.ne(query -> {
+//            query.setName("name");
+//            query.setName("pp");
+//        }).list();
+//
+//        lambdaCriteria = new LambdaCriteria<>(User.class, session);
+//        // 组合使用
+//        // sselect * from user where open =1 and name like '%xx%' and sex <> '女'
+//        lambdaCriteria
+//                .eq(query -> query.setOpen(true))
+//                .like(query -> query.setName("%xx%"))
+//                .ne(query -> query.setSex("女")).list();
+////        分页  起始值 1
+//        lambdaCriteria = new LambdaCriteria<>(User.class, session);
+//        lambdaCriteria
+//                .eq(query -> query.setOpen(true))
+//                .like(query -> query.setName("%xx%"))
+//                .ne(query -> query.setSex("女")).list(1,10);
     }
 }
